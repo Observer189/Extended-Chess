212d43013686b63703b46bfd47662cbdf26e78d6
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2022-12-28 20:16:25 +0300

    game result public attributes

--- lib/GameResult.rb
+++ lib/GameResult.rb
@@ -1,5 +1,5 @@
 class GameResult
-
+  attr_reader :winners, :losers, :game_log
   def initialize(winners,losers, log)
     @winners = winners #format: [игрок :Player, результат игры для этого игрока: string]
     @losers = losers
